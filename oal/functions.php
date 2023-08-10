d69386336a9fbcc4f293343507e37fe2f30039b7
Andreas E. Eriksen <AndreasEriksen99@hotmail.com>
2023-08-10 03:23:15 +0200

    init

--- oal/functions.php
+++ oal/functions.php
@@ -67,49 +67,94 @@ function oal_display_page() {
             <?php submit_button('Save Car'); ?>
         </form>
 
-            <!-- Hierarchical display area -->
-            <h2>All Cars</h2>
-                <div id="oal-display-area">
-                    <table class="widefat" id="oal-cars-table">
-                        <thead>
-                            <tr>
-                                <th>ID</th>
-                                <th>Username</th>
-                                <th>Brand</th>
-                                <th>Model</th>
-                                <th>Description</th>
-                                <th>Price</th>
-                                <th>Image</th>
-                                <th>Actions</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <?php
-                            if (empty($cars)) {
-                                echo '<tr><td colspan="8">No cars found.</td></tr>';
-                            } else {
-                                foreach ($cars as $username => $brands) {
-                                    foreach ($brands as $brand => $models) {
-                                        foreach ($models as $model => $car) {
-                                            echo '<tr>';
-                                            echo '<td>' . esc_html($car['id']) . '</td>';
-                                            echo '<td>' . esc_html($username) . '</td>';
-                                            echo '<td>' . esc_html($brand) . '</td>';
-                                            echo '<td>' . esc_html($model) . '</td>';
-                                            echo '<td>' . esc_html($car['description']) . '</td>';
-                                            echo '<td>$' . esc_html(number_format($car['price'], 2)) . '</td>';
-                                            echo '<td><img src="' . esc_url($car['image']) . '" width="50" height="50" /></td>';
-                                            echo '<td><button class="button delete-car" data-id="' . esc_attr($car['id']) . '">Delete</button></td>';
-                                            echo '</tr>';
-                                        }
-                                    }
-                                }
-                            }
-                            ?>
-                        </tbody>
-                    </table>
-                </div>
+        <h2>All Cars</h2>
+        <div id="oal-display-area">
             <?php
+            if (empty($cars)) {
+                echo '<p>No cars found.</p>';
+            } else {
+                foreach ($cars as $username => $brands) {
+                    echo '<div class="user-card">';
+                    echo '<h3>' . esc_html($username) . '</h3>';
+
+                    foreach ($brands as $brand => $models) {
+                        echo '<div class="brand-card">';
+                        echo '<h4>' . esc_html($brand) . '</h4>';
+
+                        foreach ($models as $model => $car) {
+                            echo '<div class="model-card">';
+                            echo '<h5>' . esc_html($model) . '</h5>';
+                            echo '<p>Description: ' . esc_html($car['description']) . '</p>';
+                            echo '<p>Price: $' . esc_html(number_format($car['price'], 2)) . '</p>';
+                            echo '<p>Image: <img src="' . esc_url($car['image']) . '" width="50" height="50" /></p>';
+                            echo '<button class="button delete-car" data-id="' . esc_attr($car['id']) . '">Delete</button>';
+                            echo '</div>';  // End of model card
+                        }
+
+                        echo '</div>';  // End of brand card
+                    }
+
+                    echo '</div>';  // End of user card
+                }
+            }
+            ?>
+        </div>
+
+        <form method="POST">
+            <?php wp_nonce_field('oal_purge_data_action', 'oal_purge_data_nonce'); ?>
+            <button type="submit" name="oal_purge_data" class="button button-primary">Purge All Data</button>
+        </form>
+
+
+
+        <style>
+            #oal-display-area {
+                display: flex;
+                flex-wrap: wrap;
+            }
+
+            .user-card, .brand-card, .model-card {
+                padding: 15px;
+                border: 1px solid #ddd;
+                margin: 10px;
+                border-radius: 5px;
+                box-shadow: 1px 1px 3px rgba(0, 0, 0, 0.1);
+            }
+
+            .user-card {
+                background-color: #f3f4f6;
+                width: 100%;
+            }
+
+            .brand-card {
+                background-color: #e2e2e2;
+                margin-left: 20px;
+            }
+
+            .model-card {
+                background-color: #d1d1d1;
+                margin-left: 40px;
+            }
+        </style>
+        <script>
+                document.addEventListener('DOMContentLoaded', function() {
+                    var brandCards = document.querySelectorAll('.brand-card h4');
+                    brandCards.forEach(function(brandCard) {
+                        brandCard.addEventListener('click', function() {
+                            var modelCards = this.parentElement.querySelectorAll('.model-card');
+                            modelCards.forEach(function(modelCard) {
+                                if (modelCard.style.display === 'none' || modelCard.style.display === '') {
+                                    modelCard.style.display = 'block';
+                                } else {
+                                    modelCard.style.display = 'none';
+                                }
+                            });
+                        });
+                    });
+                });
+        </script>
+    </div>
+    <?php
 }
 
 
@@ -199,9 +244,10 @@ function oal_get_cars() {
 
 
 function oal_enqueue_scripts($hook) {
-    if ('toplevel_page_custom-car-dashboard' !== $hook) {
+    if ('toplevel_page_oal-dashboard' !== $hook) {
         return;
     }
+    
     wp_enqueue_script('oal-admin', plugins_url('oal-admin.js', __FILE__), array('jquery'), '1.0.0', true);
     wp_localize_script('oal-admin', 'oal_ajax', array('url' => admin_url('admin-ajax.php'), 'nonce' => wp_create_nonce('oal_nonce'), 'capability' => current_user_can('activate_plugins') ? 'yes' : 'no'));
 }
@@ -210,7 +256,7 @@ add_action('admin_enqueue_scripts', 'oal_enqueue_scripts');
 
 
 function oal_delete_car() {
-    if (!wp_verify_nonce($_POST['nonce'], 'oal_nonce') || !current_user_can('activate_plugins')) {
+    if (!wp_verify_nonce($_POST['nonce'], 'oal_nonce')) {
         wp_send_json_error('Forbidden');
     }
 
@@ -221,7 +267,7 @@ function oal_delete_car() {
     foreach ($cars as $username => $brands) {
         foreach ($brands as $brand => $models) {
             foreach ($models as $model => $car) {
-                if ($car['id'] === $id) {
+                if ($car['id'] == $id) {  // Changed to non-strict comparison
                     unset($cars[$username][$brand][$model]);
                     
                     // If no more models for the brand, remove the brand.
@@ -249,16 +295,18 @@ function oal_delete_car() {
 add_action('wp_ajax_oal_delete_car', 'oal_delete_car');
 
 
-
-function oal_purge_data() {
-    check_ajax_referer('oal_nonce', 'nonce');
-    if (!current_user_can('activate_plugins')) {
-        wp_send_json_error('Forbidden');
+if (isset($_POST['oal_purge_data'])) {
+    $deleted = delete_option('oal_cars');
+    if ($deleted) {
+        add_action('admin_notices', 'oal_purge_data_notice');
+    } else {
+        add_action('admin_notices', 'oal_purge_data_error_notice');
     }
-
-    update_option('oal_cars', array());
-    wp_send_json_success();
 }
 
 
-add_action('wp_ajax_oal_purge_data', 'oal_purge_data');
+function oal_purge_data_error_notice() {
+    echo '<div class="notice notice-error is-dismissible">
+        <p>Error purging the data. Please try again.</p>
+    </div>';
+}
